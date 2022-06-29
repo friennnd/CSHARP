41bb72a035c7f96e1a61c4256513e82f1a3ef912
alex <89261066159@mail.ru>
2022-06-29 21:39:20 +0300

    ок

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите любое целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else 
+{
+    while (num > 999)
+    {   
+       num = num/10;
+       
+    }
+    int res = num % 10;
+    Console.WriteLine(res);
+}
