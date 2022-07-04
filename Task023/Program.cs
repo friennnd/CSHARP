a4af5b0c234b0390a368345ae731b4db87a7c24a
alex <89261066159@mail.ru>
2022-07-04 11:00:12 +0300

    ok23

---
+++ Task023/Program.cs
@@ -0,0 +1,22 @@
+﻿//Программа, которая принимает 
+//на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Программа выдает таблицу кубов чисел от 1 до числа введенного пользователем.");
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int cub = 0;
+if (num < 0)
+{
+    while (count >= num)
+    {
+        cub = count * count * count;
+        Console.WriteLine($"{count,3} -> {cub,5}");
+        count--;
+    }
+}
+else while (count <= num)
+    {
+        cub = count * count * count;
+        Console.WriteLine($"{count,3} -> {cub,5}");
+        count++;
+    }
