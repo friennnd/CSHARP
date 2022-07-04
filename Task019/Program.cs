c02b67d65f18c5790fc07e60dd50804aac2e2ad5
alex <89261066159@mail.ru>
2022-07-04 10:48:18 +0300

    ok119

---
+++ Task019/Program.cs
@@ -0,0 +1,24 @@
+﻿//Программа, которая принимает на вход пятизначное число
+//и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Программа показывает является ли введенное число полиндромом.");
+Console.Write("Введите пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool Palindrome(int n)
+{
+    while (n < 10000 || n > 99999)
+    {
+        Console.Write("Введено не пятизначное число. Попробуйте еще раз ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    if (n / 10000 == n % 10 && n / 1000 == num % 100)
+    {
+        return true;
+    }
+    else return false;
+}
+if (Palindrome(num) == true)
+{
+    Console.WriteLine($"Введенной число {num} полиндром");
+}
+else Console.WriteLine($"Введенной число {num} не полиндром");
