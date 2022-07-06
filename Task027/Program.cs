774eea31e59a61778b73b0fc8f7a2d44e892cbbf
alex <89261066159@mail.ru>
2022-07-06 19:25:50 +0300

    ok27

---
+++ Task027/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int DigitsCount(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int res = DigitsCount(a); 
+while (res > 9)
+{
+    res = DigitsCount(res);
+}
+System.Console.WriteLine(res);
