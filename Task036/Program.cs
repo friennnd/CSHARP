52b67cec4b8a457a3edb5f5855182b2d25393db3
alex <89261066159@mail.ru>
2022-07-11 11:45:57 +0300

    ok36

---
+++ Task036/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] CreatArray(int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) //печатает массив
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) System.Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else System.Console.Write(array[i] + "]");
+    }
+}
+
+int SumOddElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+//начало программы
+Console.WriteLine("Программа считает сумму элементов стоящих на нечетных позициях в массиве");
+Console.Write("Задайте длинну массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте нужный диапазон чисел.");
+Console.Write("Нижняя граница диапазона: ");
+int begin = Convert.ToInt32(Console.ReadLine());
+Console.Write("Верхняя граница диапазона: ");
+int end = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreatArray(size, begin, end);//задаем массив
+PrintArray(array);//печатаем заданный массив
+System.Console.Write(" -> ");//печатаем разделитель
+System.Console.Write(SumOddElement(array));//печатаем ответ
